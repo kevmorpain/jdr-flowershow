ed90d277cb5c9a5c5c38000044609190d535b7f9
Kévin Morpain <kevmorpain@gmail.com>
2023-04-01 11:20:51 +0200

    change scripts to force export

--- .flowershow/out/_next/static/ha7kVDXw1sVPWga1op0eZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_all":["static/chunks/pages/_all-6b7a0a8b7d1087b9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],"/[[...slug]]":["static/chunks/pages/[[...slug]]-78b998aaefbd2926.js"],sortedPages:["/_all","/_app","/_error","/[[...slug]]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
