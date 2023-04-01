a61f494b6862425bf4d285993254e8c47bb4b85d
Kévin Morpain <kevmorpain@gmail.com>
2023-04-01 02:34:40 +0200

    fix nextjs config

--- .flowershow/next.config.mjs
+++ .flowershow/next.config.mjs
@@ -1,6 +1,20 @@
 import { withContentlayer } from "next-contentlayer";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = '/'
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 export default withContentlayer({
+  basePath,
+  assetPrefix,
   eslint: {
     ignoreDuringBuilds: true,
   },
